b64a5ddf399d6a08452109681c473bed939b1461
Vladimir Ivanov <ivasin75@mail.ru>
2020-11-07 22:37:28 +0300

    add all

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,5 +1,6 @@
 import com.sun.xml.internal.ws.api.policy.subject.BindingSubject;
 
+import javax.swing.plaf.synth.SynthTabbedPaneUI;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,20 +8,29 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        String host = "127.0.0.1";
+        String host = "netology.workhost";
         int serverPort = 8080;
-        Socket socket = new Socket(host, serverPort);
+        Scanner scan = new Scanner(System.in);
+        Socket socket = new Socket("127.0.0.1", serverPort);
         //отправка данных серверу
         PrintWriter outServer = new PrintWriter(socket.getOutputStream(), true);
-        outServer.println("Привет сервер!");
         // получение данных с сервера
         BufferedReader inServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        System.out.println(inServer.readLine());
+        String msg;
+            outServer.println("Привет сервер!");
+        System.out.println("SERVER: " + inServer.readLine());
+            while (!(inServer.readLine()==null)) {
+                System.out.println("SERVER: " + inServer.readLine());
+                msg = scan.nextLine();
+            outServer.println(msg);
+            if ("end".equals(msg)) break;
+            System.out.println("SERVER: " + msg);
+        }
+
         inServer.close();
-
     }
-
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,58 @@
+import javafx.scene.transform.Scale;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.lang.reflect.Array;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketTimeoutException;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        String userName = null;
         int serverPort = 8080;
         ServerSocket serverSocket = new ServerSocket(serverPort);
         Socket server = serverSocket.accept();
         //получение данных от клиента
         BufferedReader inClient = new BufferedReader(new InputStreamReader(server.getInputStream()));
-        String line = inClient.readLine();
-        System.out.println(line);
-        //передача данных клиенту
         PrintWriter outClient = new PrintWriter(server.getOutputStream(), true);
-        outClient.println("Привет клиент " + server.getLocalSocketAddress());
+        outClient.println("Соединение с " + server.getLocalSocketAddress() + " установлено");
+        String messageClient = inClient.readLine();
+        System.out.println(messageClient);
+//        while (!server.isClosed()) {
+            outClient.println("Как тебя зовут?");
+            System.out.println(inClient.readLine());
+            userName = inClient.readLine();
+            outClient.println("Привет " + userName + "!");
+            outClient.println("Если Вы не робот, введите число от 1 до 10");
+            System.out.println(inClient.readLine());
+            try {
+                if (Integer.getInteger(inClient.readLine()) > 0 || Integer.getInteger(inClient.readLine()) < 11)
+                    outClient.println("Добро пожаловать, " + userName + "!");
+            } catch (NullPointerException e) {
+                outClient.println("Вы ввели некорректное значение, повторите");
+                messageClient = inClient.readLine();
+            }
+        outClient.println("Ты еще ребенок? (да/нет)");
+        messageClient = inClient.readLine();
+        while (!messageClient.equals("да")) {
+            if ("нет".equals(messageClient)) {
+                outClient.println("Привет! " + userName + ", Вы попали на сайт налоговой службы! " +
+                        "Давайте Ваши денежки!");
+            } else {
+                outClient.println("Введите корректное значение");
+                messageClient = inClient.readLine();
+                continue;
+            }
+            outClient.println("Извини, этот сайт для взрослых, До свидания");
+        }
+            server.close();
+//    }
+}}
 
-    }
-}
+
+
