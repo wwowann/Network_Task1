3876c30b07a87b5487331fd6576a2b778b11e4ce
Vladimir Ivanov <ivasin75@mail.ru>
2020-11-09 16:16:59 +0300

    add all

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -2,27 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-//        String host = "netology.workhost";
+        String host = "netology.homework";
         int serverPort = 8080;
+//        InetAddress inetAddress = InetAddress.getByName(host);
+        Socket socket = new Socket(host, serverPort);
         Scanner scan = new Scanner(System.in);
-        Socket socket = new Socket("127.0.0.1", serverPort);
+        socket.setSoTimeout(10000);
         //отправка данных серверу
         PrintWriter outServer = new PrintWriter(socket.getOutputStream(), true);
         // получение данных с сервера
         BufferedReader inServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         String msg;
-        while (!socket.isClosed()) {
-            System.out.println("SERVER: " + inServer.readLine());
-            msg = scan.nextLine();
-            outServer.println(msg);
-            outServer.flush();
-            if ("end".equals(msg)) break;
+        try {
+            while (!socket.isClosed() && socket.isConnected()) {
+                System.out.println("SERVER: " + inServer.readLine());
+                msg = scan.nextLine();
+                outServer.println(msg);
+                if ("end".equals(msg)) break;
+            }
+        } catch (SocketException e) {
+            System.out.println("Сервер закончил свою работу");
         }
         inServer.close();
+        outServer.close();
+        socket.close();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,43 +12,48 @@ public class Main {
         int serverPort = 8080;
         ServerSocket serverSocket = new ServerSocket(serverPort);
         Socket server = serverSocket.accept();
+        server.setSoTimeout(10000);
         //получение данных от клиента
         BufferedReader inClient = new BufferedReader(new InputStreamReader(server.getInputStream()));
         //отправка данных клиенту
         PrintWriter outClient = new PrintWriter(server.getOutputStream(), true);
         boolean bool = true;
-        outClient.println("Привет! Как тебя зовут?");
-        userName = inClient.readLine();
-        outClient.println("Привет " + userName + "! Если Вы не робот, введите число от 1 до 10");
-        String msg;
-        while (bool) {
-            msg = inClient.readLine();
-            try {
-                int cod = Integer.parseInt(msg.trim());
-                if (cod > 0 && cod < 11) {
-                    outClient.println("Добро пожаловать, " + userName + "! Ты еще ребенок? (да/нет)");
-                    bool = false;
-                } else throw new NumberFormatException("Вы ввели некорректное значение, повторите ввод");
-            } catch (NumberFormatException e) {
-                outClient.println("Вы ввели некорректное значение, повторите ввод");
-            }
-        }
-        msg = inClient.readLine();
-        while (!serverSocket.isClosed()) {
-            if (msg.equals("да")) {
-                outClient.println("Извини, этот сайт для взрослых, До свидания! Сервер прощается с Вами. всего хорошего!");
-                serverSocket.close();
-            } else if (msg.equals("нет")) {
-                outClient.println(userName + ", Вы попали на сайт налоговой службы! " +
-                        "Давайте Ваши денежки!");
-                serverSocket.close();
-            } else {
-                outClient.println("Введите корректное значение");
+        try {
+            outClient.println("Привет! Как тебя зовут?");
+            userName = inClient.readLine();
+            outClient.println("Привет " + userName + "! Если Вы не робот, введите число от 1 до 10");
+            String msg;
+            while (bool) {
                 msg = inClient.readLine();
+                try {
+                    int cod = Integer.parseInt(msg.trim());
+                    if (cod > 0 && cod < 11) {
+                        outClient.println("Добро пожаловать, " + userName + "! Ты еще ребенок? (да/нет)");
+                        bool = false;
+                    } else throw new NumberFormatException("Вы ввели некорректное значение, повторите ввод");
+                } catch (NumberFormatException e) {
+                    outClient.println("Вы ввели некорректное значение, повторите ввод");
+                }
             }
+            msg = inClient.readLine();
+            while (!serverSocket.isClosed()) {
+                if (msg.equals("да")) {
+                    outClient.println("Извини, этот сайт для взрослых, До свидания! Сервер прощается с Вами. всего хорошего!");
+                    serverSocket.close();
+                } else if (msg.equals("нет")) {
+                    outClient.println(userName + ", Вы попали на сайт налоговой службы! " +
+                            "Давайте Ваши денежки!");
+                    serverSocket.close();
+                } else {
+                    outClient.println("Введите корректное значение");
+                    msg = inClient.readLine();
+                }
+            }
+            outClient.println("Сервер прощается с Вами. Всего хорошего!");
+            serverSocket.close();
+        }catch (SocketTimeoutException e){
+            System.out.println("Время отклика клиента закончилось. Соединение разорвано");
         }
-        outClient.println("Сервер прощается с Вами. всего хорошего!");
-        serverSocket.close();
     }
 }
 
